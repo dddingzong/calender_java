d0862fe8355957cff3353a37650c39efd1762542
82108 <82108@DESKTOP-D518FSA>
2022-06-28 20:13:18 +0900

    완성

--- src/sum.java
+++ src/sum.java
@@ -1,3 +1,19 @@
-public class sum {
+import java.util.Scanner;
 
+
+
+public class sum {
+	public static void main(String[] args) {
+		System.out.println("두 수를 입력하새요.");
+		
+		Scanner sc = new Scanner(System.in);
+
+	    String inputValue = sc.nextLine();
+	    String[] splitedValue = inputValue.split(" ");
+	    int a = Integer.parseInt(splitedValue[0]);
+	    int b = Integer.parseInt(splitedValue[1]);
+	       
+	    System.out.println("두 수의 합은 " + (a+b) + "입니다.");
+	    
+	}
 }
