cbb7c96b2ec863d32730a0182080b0981a8f99c2
82108 <82108@DESKTOP-D518FSA>
2022-06-29 13:23:11 +0900

    완성

--- src/calender2.java
+++ src/calender2.java
@@ -1,23 +1,22 @@
 import java.util.Scanner;
 
 public class calender2 {
-	
-	private static final int[] date = {31,28,31,30,31,30,31,31,30,31,30,31};
-	
+
+	private static final int[] date = { 31, 28, 31, 30, 31, 30, 31, 31, 30, 31, 30, 31 };
+
 	public int maxdays(int month) {
-		return date[month-1];
+		return date[month - 1];
 	}
-	
+
 	public static void main(String[] args) {
-		
+
 		System.out.println("달을 입력하세요");
 		Scanner sc = new Scanner(System.in);
 		calender2 cal = new calender2();
 		int month = sc.nextInt();
-		
-		System.out.println(month+"월은 "+cal.maxdays(month)+"일까지 있습니다.");
-		
+
+		System.out.println(month + "월은 " + cal.maxdays(month) + "일까지 있습니다.");
+
 		sc.close();
 	}
 }
-
