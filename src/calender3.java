48f65945201f8d6f77dfa814e9046569ab0c6f21
82108 <82108@DESKTOP-D518FSA>
2022-06-30 20:52:37 +0900

    완성

--- src/calender3.java
+++ src/calender3.java
@@ -4,14 +4,29 @@ public class calender3 {
 	public static void main(String[] args) {
 		
 		int[] date = { 31, 28, 31, 30, 31, 30, 31, 31, 30, 31, 30, 31 };
-
-		System.out.println("달을 입력하세요");
+		int[] month_list = new int[12];
+		
+		System.out.println("반복횟수를 입력하세요.");
 		Scanner sc = new Scanner(System.in);
-		calender2 cal = new calender2();
-		int month = sc.nextInt();
-
-		System.out.println(month + "월은 " + date[month - 1] + "일까지 있습니다.");
-
+		int iter = sc.nextInt();
+		
+		System.out.println("");
+		
+		System.out.println("월을입력하세요");
+		
+		for (int i=0;i<iter;i++) {
+			int month = sc.nextInt();
+			month_list[i] = month;
+		}
+		
+		System.out.println("");
+		
+		for (int i=0;i<iter;i++) {
+		System.out.println(month_list[i] + "월은 " + date[month_list[i] - 1] + "일까지 있습니다.");
+		}
+		
+		System.out.println("끝!");
+		
 		sc.close();
 	}
 }
